84d3506475f3d592af52d2b237e65097226bc608
srishanthpatel <134792134+srishanthpatel@users.noreply.github.com>
2025-06-05 13:23:23 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state={
+    img:"",
+    name:"",
+    bio:""
+  }
+
+eventsubmitted=(event)=>{
+event.preventDefault()
+this.postapirequest()
+}
+
+postapirequest= async()=>{
+  const url="https://apitesting-qcys.onrender.com/api/items"
+  const {img,name,bio}=this.state
+  const options={
+    method:'POST',
+    headers:{
+      "Content-Type": "application/json"
+    },
+    body:JSON.stringify({img,name,bio})
+  }
+  const data= await fetch(url,options)
+  const response= await data.json()
+  console.log(response)
+}
+
+inpchanged=(event)=>{
+  this.setState({img:event.target.value})
+}
+namechanged=(event)=>{
+  this.setState({name:event.target.value})
+}
+biochanged=(event)=>{
+  this.setState({bio:event.target.value})
+}
+
+  render() {
+    return (
+      <div className="app-container">
+        <h1 className="form-title">Make An API Request</h1>
+        <form className="form-container" onSubmit={this.eventsubmitted}>
+          <label htmlFor="imgUrl">Image URL</label>
+          <input id="imgUrl" type="text" placeholder="Enter image URL" onChange={this.inpchanged}/>
+
+          <label htmlFor="name">Name</label>
+          <input id="name" type="text" placeholder="Enter name" onChange={this.namechanged}/>
+
+          <label htmlFor="desc">Description</label>
+          <input id="desc" type="text" placeholder="Enter description" onChange={this.biochanged}/>
+
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
